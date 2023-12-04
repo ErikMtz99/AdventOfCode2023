949632ee649f5501d78b7267cc76dbb0bc92530b
Erik Martinez <ferik.mtz@outlook.com>
2023-12-03 20:54:38 -0600

    Updating file name and removing extra file

--- day1.py
+++ day1.py
@@ -1,15 +1,34 @@
+def find_number(arg):
+   numeros = []
+   for x in arg:
+       if(x.isnumeric()):
+           numeros.append(int(x))
+   valor = int(str(numeros[0]) + str(numeros[-1]))
+   numeros.clear()
+   return int(valor)
+
+def change_word_to_num(arg):
+    word_nums = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+    new_word = arg
+    
+    for idx, num in enumerate(word_nums, 1):
+        if((new_word.find(num,0)) >= 0):
+            new_word = new_word.replace(num, str(idx))
+            
+    valor = find_number(new_word) 
+    #f.write(str(valor))
+    #f.write("\n")
+    return valor
 
 total = 0
-lista1 = open("day1.txt", "r")
-numeros = []
+list1 = open("inputs/day1.txt", "r")
+#f = open("prueba.txt", "w")
 
-for elemento in lista1:
-    for x in elemento:
-        if(x.isnumeric()):
-            numeros.append(int(x))
-    valor = int(str(numeros[0]) + str(numeros[-1]))
-    numeros.clear()
-    total += valor
-
-print(numeros)   
+for element in list1:
+    #Part 1 solution
+    #total += find_number(element) 
+    
+    #Part 2 solution
+    total += change_word_to_num(element) 
 print(total)
+#f.close()
